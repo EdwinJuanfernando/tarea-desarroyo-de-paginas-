95ada364ee08450ee01f0921e87659768484ac5a
EdwinJuanfernando <edwinjuan1500@gmail.com>
2025-03-19 15:52:06 -0600

    Add files via upload

---
+++ tarea.py
@@ -0,0 +1,23 @@
+''' Cada un numero de 82 cifras convertir su equivalente a texto
+15 noventa y nueve
+30 treinta
+'''
+numeros_en_texto = [
+"cero", "una", "des", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve",
+"veinte", "veint line", "veintidis", "velntitrés", "veinticuatro", "veinticinco", "veintisels", "veintistete", "veintiocho", "veintinueve", "treinta
+"dier", "ance, dace", "trece", "catorce", "quince", "dieciséis", "diecisiete", "dieciocho", "diecinueve",
+]
+decenas_en_texto = ['cera", "diez", "veinte", "treinta", "cuarenta", "cincuenta", "sesenta", "setenta", "ochenta", "noventa", "cien"']
+print(instrucciones)
+numeraint(input("Ingrese un numero: "))
+numero
+print('Mmaro ingresado: (numero) (numeros_on_texto[numero]}')
+numero 1 numera 301
+print('Numere ingresado: (numero) (numeros_en_texto[numero]}')
+numero 31 пищего 9:
+unidad nunero 10
+Porintiunidad)
+#print(decona)f Numer
+print(f' ingresada: (numero) (decenas en_texto[decena]} y (numeros_en_texto[unidad]}')
+elif numero >= 100: 
+print('Mumero no valide, ingreso (numero)')
