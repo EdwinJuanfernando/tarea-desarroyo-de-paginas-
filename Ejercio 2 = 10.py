b7477a5663cfbaa58cdbe1dca84b72b8dedeb1cb
EdwinJuanfernando <edwinjuan1500@gmail.com>
2025-04-08 15:54:09 -0600

    Add files via upload

---
+++ Ejercio 2 = 10.py
@@ -0,0 +1,12 @@
+def main():
+    tamaño = int(input("Por favor, ingrese el tamaño de los vectores: "))
+    nombres = []
+    longitudes = []
+    for i in range(tamaño):
+        nombre = input(f"Ingrese el nombre {i+1}: ")
+        nombres.append(nombre)
+        longitudes.append(len(nombre))
+    print(f"Array1 (Nombres): {nombres}")
+    print(f"Longitud (Longitudes de los nombres): {longitudes}")
+if _name_ == "_main_":
+    main()
